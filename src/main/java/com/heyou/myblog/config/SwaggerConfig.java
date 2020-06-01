88801a314bd429de61ae15bb28609d31243b033c
heyou-macbook <heyou_0423@163.com>
2020-06-01 22:48:40 +0800

    add:swagger 和拦截器配置

---
+++ src/main/java/com/heyou/myblog/config/SwaggerConfig.java
@@ -0,0 +1,65 @@
+package com.heyou.myblog.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * swagger-ui配置
+ * @author heyou
+ * @date 2019/6/14 0014
+ */
+//标记配置类
+@Configuration
+//开启在线接口文档
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .globalOperationParameters(setHeaderToken())
+                // 选择那些路径和api会生成document
+                .select()
+                // 对所有api进行监控
+                .apis(RequestHandlerSelectors.any())
+                //不显示错误的接口地址
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                // 对根下所有路径进行监控
+                .paths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("标题：何友的接口文档")
+                .description("描述：Swagger接口文档")
+                .contact(new Contact("heyou", null, null))
+                .version("版本号:1.0")
+                .build();
+    }
+}
---
+++ src/main/java/com/heyou/myblog/config/WebMvcConfig.java
@@ -0,0 +1,33 @@
+package com.heyou.myblog.config;
+
+import com.heyou.myblog.interceptor.LoginHandlerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 拦截器配置
+ * @author heyou
+ * @date 2019-11-21 17:15
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    //将组件注册在容器中
+    @Bean
+    public WebMvcConfigurer webMvcConfigurerAdapter(){
+        return new WebMvcConfigurer(){
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //静态资源； *.css,*.js
+                //SpringBoot已经做好了静态资源映射
+//                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+//                .excludePathPatterns("/index.html","/","/user/login","/static/**","/webjars/**");
+                // /**  表示拦截所有路径下的所有请求
+                registry.addInterceptor(new LoginHandlerInterceptor())
+                        .addPathPatterns("/user/**");
+            }
+        };
+    }
+}
